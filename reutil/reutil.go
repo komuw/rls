605d79386435d37a69fc6afae9a0680d6165e9dd
komuw <komuw05@gmail.com>
2025-04-28 15:37:50 +0300

    f

--- reutil/reutil.go
+++ reutil/reutil.go
@@ -25,7 +25,6 @@ func Join(quote bool, strs ...string) string {
 // Build builds a regexp for strings.
 //
 // Config options:
-//
 //	i - ignore case
 //	^ - add ^ start anchor
 //	a - add \b start anchor
